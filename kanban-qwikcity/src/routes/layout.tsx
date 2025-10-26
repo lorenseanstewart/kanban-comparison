39ebd5046be8871b65cd90d35a61ed82978a8308
lstew111 <loren.stewart@engelvoelkers.com>
2025-10-25 17:50:23 -0700

    removed unneeded component

--- kanban-qwikcity/src/routes/layout.tsx
+++ kanban-qwikcity/src/routes/layout.tsx
@@ -1,17 +1,11 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <div data-theme="pastel" class="min-h-screen bg-base-300 text-base-content">
-      <nav class="navbar bg-base-100 shadow-lg mb-8">
-        <div class="flex-1">
-          <Link href="/" class="btn btn-ghost text-xl">
-            Kanban Boards
-          </Link>
-        </div>
-      </nav>
-
+    <div
+      data-theme="pastel"
+      class="min-h-screen bg-base-300 text-base-content"
+    >
       <div class="container mx-auto px-4">
         <Slot />
       </div>
